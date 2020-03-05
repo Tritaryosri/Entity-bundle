37ac3dd31d75bee6c49302ed03b9da03e11d3539
Yosri Triter <yosri.triter@dotit-medu.com>
2020-03-05 10:30:29 +0100

    commit

---
+++ src/Entity/Traits/ObjectMetaDataTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dotit\CashpointBundle\Entity\Traits;
+
+/**
+ * Trait ObjectMetaDataTrait
+
+ * @author Tritar Yosri
+ */
+trait ObjectMetaDataTrait
+{
+    /**
+     * Getting object short class name
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
+    public function getClass()
+    {
+        return (new \ReflectionClass($this))->getShortName();
+    }
+}
